a37bf3d9f1ee0fa24030da1a7141b235ce5b2bbf
Elisa <elisat15@uw.edu>
2020-07-30 10:38:20 -0700

    example

---
+++ data-prog/example.py
@@ -0,0 +1,4 @@
+
+# reading + traversing through a file
+
+# writing to a specific file
---
+++ web-dev/script.js
@@ -0,0 +1,26 @@
+// Factor out the code below to use a for loop:
+console.log(1 + 2);
+console.log(1 + 3);
+console.log(1 + 4);
+console.log(1 + 5);
+console.log(1 + 6);
+
+// Factor out the code below to use a for loop:
+console.log(1 + 2);
+console.log(1 + 4);
+console.log(1 + 6);
+console.log(1 + 8);
+console.log(1 + 10);
+
+// Add the class "red" to add a background color 
+// of red to each DOM element with the class "villager"
+
+
+// Create an array containing 6 colors assigning it the variable 
+// name favColors
+
+
+// Using the array you made previously, create <div> that 
+// have the innerHTML equal to &nbps; and have the class .box 
+// style the background color of the <div> to equal one of the colors in the
+// favColors array
